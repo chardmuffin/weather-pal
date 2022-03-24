6c27933024ecb50b6c6b0c0f1dee2a629a23d86f
Richard Huffman <richardhuffman96@gmail.com>
2022-03-24 18:00:20 -0400

    got a successful response from the API

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
 var userFormEl = document.querySelector("#user-form");
 var historyButtonsEl = document.querySelector("#history-buttons");
 var cityInputEl = document.querySelector("#city");
-var weatherSummaryContainerEl = document.querySelector("#weather-summary-container");
+var APIKey = "d9e5ba7e4082c6b542e25c028b4913b6";
 
 var formSubmitHandler = function(event) {
   // prevent page from refreshing
@@ -14,7 +14,12 @@ var formSubmitHandler = function(event) {
     getWeather(cityName);
 
     // clear old content
-    weatherSummaryContainerEl.textContent = "";
+    document.querySelector("#weather-summary-container .card-header").textContent = "-----------------";
+    document.querySelector("#weather-icon").className = "";
+    document.querySelector("#temp-value").textContent = "---";
+    document.querySelector("#wind-value").textContent = "---";
+    document.querySelector("#humidity-value").textContent = "---";
+    document.querySelector("#uv-index").textContent = "---";
     cityInputEl.value = "";
   } else {
     alert("Please enter a city");
@@ -24,17 +29,17 @@ var formSubmitHandler = function(event) {
 var buttonClickHandler = function(event) {
 
   //get the city name from the button
-  console.log(event.target)
+  city = event.target.textContent.trim();
 
   getWeather(city);
 };
 
 var getWeather = function(city) {
   // format the weather api url
-  var apiUrl = "";
+  var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
 
   // make a get request to url
-  fetch(apiUrl)
+  fetch(queryURL)
     .then(function(response) {
       // request was successful
       if (response.ok) {
